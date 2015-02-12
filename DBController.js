a0a1c43f2436ff7b58d049aaae7546fc64e07124
ruks <ruky68@live.com>
2015-02-12 17:33:46 +0530

    acc remove fixed

--- DBController.js
+++ DBController.js
@@ -183,6 +183,31 @@ DBController.prototype.loadAccount=function(userName){
     }
 }
 
+DBController.prototype.deleteAccount=function(id,func){
+  var objectStore = this.account_database.transaction([this.accountTableName],'readwrite').objectStore(this.accountTableName);
+  var req=objectStore.openCursor();
+
+   req.onsuccess = function(e) { 
+   		var cursor = e.target.result;
+ 		if(cursor){
+ 			if(cursor.key==id){
+	 			var request = cursor.delete();
+		        request.onsuccess = function() {
+		          	if(func){
+	 					func();
+	 				}
+		        }; 			
+		        return;
+ 			} 
+ 			cursor.continue();			
+ 		}
+   }
+
+   req.onerror = function(event) {
+    	console.log(event.target.errorCode);    	
+    };
+}
+
 DBController.prototype.loadAccountById=function(id,func){
   var objectStore = this.account_database.transaction(this.accountTableName).objectStore(this.accountTableName);
   var req=objectStore.get(id);
